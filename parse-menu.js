7980a0916a3eb703c2d52bdb5ff90e11005df12e
Steven Xu <weareallhalfminotaur@gmail.com>
2021-01-30 21:59:58 -0600

    add dictionary to store food categories

--- parse-menu.js
+++ parse-menu.js
@@ -1,6 +1,9 @@
-function RenderMenu() {
-    // Get JSON for the menu information
-    ret = $.ajax({
+var menuHTML;
+var dishDict = {};
+
+function renderMenu() {
+    // Get JSON for the menu information from Bowdoin API
+    response = $.ajax({
         type: "POST",
         url: "https://apps.bowdoin.edu/orestes/api.jsp",
         data: {
@@ -11,30 +14,61 @@ function RenderMenu() {
         },
         dataType: "html"
     })
-    
-    ret.done(function(menu) { // Loads HTML of the menu from the JSON response text
-        html = "<div id='u"+4949+"' class='unit col-md-6'>"+$(menu).html()+"</div>";
-        html = html.replace("Service Unit:", "Service Unit<br/>");
-        html = html.replace(/\<h3\>/g,'<h4>').replace(/\<\/h3\>/g,'</h4>');
-        html = html.replace('<strong>','<h3>').replace('</strong>','</h3>');
-        html = html.replace('<p class="unit"></p>','');
-        
 
-        html = html.replace('.75rem;','2rem;margin-top:1.5rem;border-top:1px solid #777;display:none;');
-        var pos = html.indexOf('<center>');
-        if (pos > -1) {
-            if (4949 === 48) {
-                html = [html.slice(0,pos), '<h3>Moulton Union Service Unit</h3>', html.slice(pos)].join('');
-            } else {
-                html = [html.slice(0,pos), '<h3>Thorne Hall Service Unit</h3>', html.slice(pos)].join('');
-            }
-            html = html.replace("<center>", '<p>').replace('</center>', '</p>').replace('<em>','').replace('</em>','');
-        }
-            $("#mbox").append(html);
-    
+    // Loads HTML of the menu from the JSON response text 
+    response.done(function (menu) {
+        menuHTML = $(menu).html();
+
+        // $("#mbox").append(menuHTML);
+
+        // console.log(menuHTML);
+        parseMenuHTML(menuHTML);
     });
-
-    // TODO: parse ret 
 }
 
-RenderMenu()
+/*
+Populates dictionary
+*/
+function parseMenuHTML(menuString) {
+    // Note: SPECIALLY STORE first category & last category!! 
+    // First category is like: `<p class="unit"> <strong>Thorne Hall  <br>  Lunch, 2021-01-31</strong> </p>`
+    // Last category is like: `<p style="font-size:.75rem;color:#999;"> <em> Salad Bar, Steamed Rice ... Menu Subject to Change.</em> </p>
+    
+    // pre-process the string for splitting into categories
+    menuString = menuHTML.replace('<p style="font-size:.75rem;color:#999;">', '<h3 style="clear:both;">')
+
+    // 1. Parse menu categories
+    const categories = menuString.split('<h3 style="clear:both;">');
+
+    // DEBUG code (excludes non-categories)
+    // for (let i = 1; i < categories.length - 1; i++) {
+    //     console.log(`Category ${i}: ${categories[i]}`);
+    // }
+
+    dishDict['header'] = categories[0]; // first category (see beginning of func for ex.)
+    dishDict['footer'] = categories[categories.length - 1]; // last category (see beginning of func for ex.)
+
+    // 2. Parse items for each category (except header & footer)
+    for (let catIdx = 1; catIdx < categories.length - 1; catIdx++) {
+        // split each item by span
+        var items = categories[catIdx].split('<span>');
+
+        // replace HTML tags + print test code
+        items[0] = items[0].replace('</h3>', '');
+        for (let itemIdx = 1; itemIdx < items.length; itemIdx++) {
+            items[itemIdx] = items[itemIdx].replace('</span>\n<br>', '');
+            // console.log(`${items[0]}: ${items[itemIdx]}`);
+        }
+        // Populate dictionry, example: {key : val} --> soup : [cookies & cream, jam & jelly]
+        dishDict[items[0]] = items.slice(1);
+    }
+    console.log(dishDict);
+}
+
+
+function returnMenuToDisplay() {
+    var menuToReturn = ""
+    
+}
+
+renderMenu();
