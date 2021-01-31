2c668e85aa2497c40642f93dba71ba444eb7e8dc
Steven Xu <weareallhalfminotaur@gmail.com>
2021-01-30 23:11:28 -0600

    add getFunMeal()

--- parse-menu.js
+++ parse-menu.js
@@ -1,16 +1,22 @@
 var menuHTML;
-var dishDict = {'Drink': ['Milk', 'Water', 'Vitamin water', 'Plain o\' water', 'Sparkling water', 'Coca-Cola', 'Root Beer', 'Sprite', 'whatever is in your waterbottle right now']};
+var foodDict = { 'Drink': ['Decaf coffee', 'Dark coffee', 'Coffee with cream, sugar, honey', 'Tea of choice', 'Iced tea', 'Lemonade', 'Juice of your choice', 'Milk', 'Mineral Water', 'Vitamin water', 'Plain o\' water', 'Sparkling water', 'Coca-Cola', 'Root Beer', 'Sprite', 'whatever is in your waterbottle right now'] };
 
-function renderMenu() {
+function renderMenu(meal) {
+    menuDate = new Date();
+    today = menuDate.toISOString().split('T')[0];
+    today = today.replace(/-/g,"");
+
+    diningHall = 49 // 49 = Thorne
+
     // Get JSON for the menu information from Bowdoin API
     response = $.ajax({
         type: "POST",
         url: "https://apps.bowdoin.edu/orestes/api.jsp",
         data: {
-            unit: 49, // 49 = Thorne
-            date: 20210131,
-            meal: "lunch",
-            tra: "guide"
+            unit: diningHall, 
+            date: today,
+            meal: meal, //breakfast, lunch, or dinner
+            tra: "guide" //preset (no idea what this is)
         },
         dataType: "html"
     })
@@ -19,9 +25,8 @@ function renderMenu() {
     response.done(function (menu) {
         menuHTML = $(menu).html();
 
-        // $("#mbox").append(menuHTML);
+        $("#mbox").append(menuHTML);
 
-        // console.log(menuHTML);
         parseMenuHTML(menuHTML);
     });
 }
@@ -33,7 +38,7 @@ function parseMenuHTML(menuString) {
     // Note: SPECIALLY STORE first category & last category!! 
     // First category is like: `<p class="unit"> <strong>Thorne Hall  <br>  Lunch, 2021-01-31</strong> </p>`
     // Last category is like: `<p style="font-size:.75rem;color:#999;"> <em> Salad Bar, Steamed Rice ... Menu Subject to Change.</em> </p>
-    
+
     // pre-process the string for splitting into categories
     menuString = menuHTML.replace('<p style="font-size:.75rem;color:#999;">', '<h3 style="clear:both;">')
 
@@ -45,8 +50,8 @@ function parseMenuHTML(menuString) {
     //     console.log(`Category ${i}: ${categories[i]}`);
     // }
 
-    dishDict['header'] = categories[0]; // first category (see beginning of func for ex.)
-    dishDict['footer'] = categories[categories.length - 1]; // last category (see beginning of func for ex.)
+    foodDict['header'] = categories[0]; // first category (see beginning of func for ex.)
+    foodDict['footer'] = categories[categories.length - 1]; // last category (see beginning of func for ex.)
 
     // 2. Parse items for each category (except header & footer)
     for (let catIdx = 1; catIdx < categories.length - 1; catIdx++) {
@@ -60,15 +65,29 @@ function parseMenuHTML(menuString) {
             // console.log(`${items[0]}: ${items[itemIdx]}`);
         }
         // Populate dictionry, example: {key : val} --> soup : [cookies & cream, jam & jelly]
-        dishDict[items[0]] = items.slice(1);
+        foodDict[items[0]] = items.slice(1);
     }
-    console.log(dishDict);
+    return foodDict;
 }
 
 
-function returnMenuToDisplay() {
-    var menuToReturn = ""
-    
+function getFunMeal() {
+    var preppedMeal = "Your meal is... (in this order):\n\n";
+
+    for (var food in foodDict) {
+        foodList = foodDict[food];
+
+        //get random num from 0 to foodList.length - 1
+        randomItemIdx = Math.floor(Math.random() * foodList.length - 1);
+
+        preppedMeal += foodList[randomItemIdx];
+        preppedMeal += "\n"
+    }
+
+    return preppedMeal;
 }
 
-renderMenu();
+renderMenu("lunch");
+console.log(menuHTML);
+// console.log(foodDict);
+// console.log(getFunMeal());
